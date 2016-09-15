dd7ba4ed24e33c7d9fd63b3653bb32e6768ecaa9
Matt Czygrinow <maciejc85@gmail.com>
2016-09-15 14:29:35 -0400

    Angular 2 2.0.0 update

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule }      from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { HttpModule, HTTP_PROVIDERS } from "@angular/http";
+import { HttpModule } from "@angular/http";
 import { routing, appRoutingProviders } from "./app.routes";
-import { AppComponent }  from "./app.component";
 import {FormsModule } from "@angular/forms";
+
+import { AppComponent }  from "./app.component";
+import { HomeComponent } from "./components/home/home";
+import { DetailsComponent } from "./components/details/details";
+import { DashboardComponent } from "./components/dashboard/dashboard";
+import { SidenavComponent } from "./components/sidenav/sidenav";
+
+import { BinarySearchService } from "./algos/binarySearch/binarySearch";
+import { MergeSortService } from "./algos/mergeSort/mergeSort";
+import { QuickSortService } from "./algos/quicksort/quicksort";
+import { StackComponent } from "./algos/stack/stack.component";
 
 @NgModule({
   imports: [BrowserModule, routing, FormsModule, HttpModule],
-  declarations: [AppComponent],
+  declarations: [ AppComponent,
+                  HomeComponent,
+                  DetailsComponent,
+                  DashboardComponent,
+                  SidenavComponent],              
   bootstrap: [AppComponent],
-  providers: [appRoutingProviders, HTTP_PROVIDERS]
+  providers: [ appRoutingProviders,
+               BinarySearchService,
+               MergeSortService,
+               QuickSortService,
+               StackComponent ]
 })
 export class AppModule { }
